a356830a15d637cf6e47bc2a805b3350e9d1e628
vanand23 <vandana1anand@gmail.com>
2019-10-18 17:55:35 -0400

    Add MATLAB output files

---
+++ Undergraduate/CS4033/vanand_HW1/HW1_script_output.m
@@ -0,0 +1,70 @@
+
+CS4033_hw1_script
+
+ans =
+
+     2     0     4
+     0     3     1
+
+
+ans =
+
+   0.500000000000000                   0
+                   0   1.000000000000000
+
+{Error using <a href="matlab:matlab.internal.language.introspective.errorDocCallback('inv')" style="font-weight:bold">inv</a>
+Matrix must be square.
+
+Error in <a href="matlab:matlab.internal.language.introspective.errorDocCallback('CS4033_hw1_script', '/Users/vandanaanand/Documents/MATLAB/CS4033_hw1_script.m', 14)" style="font-weight:bold">CS4033_hw1_script</a> (<a href="matlab: opentoline('/Users/vandanaanand/Documents/MATLAB/CS4033_hw1_script.m',14,0)">line 14</a>)
+inv(B)
+} 
+
+y =
+
+    1.3333    0.0031
+
+
+y =
+
+   1.333333333333333   0.003060019562095
+
+
+y =
+
+   1.3333e+00   3.0600e-03
+
+
+y =
+
+       4/3           44/14379 
+
+
+y =
+
+    1.3333    0.0031
+
+
+ans =
+
+    11
+
+
+ans =
+
+     2
+
+  Columns 1 through 8
+
+         0    0.4055    0.6931    0.9163    1.0986    1.2528    1.3863    1.5041
+
+  Columns 9 through 11
+
+    1.6094    1.7047    1.7918
+
+   2.2204e-16
+
+
+ans =
+
+   2.2204e-16
+
