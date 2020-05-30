46c8c490a708c80654e9370d9331aaa6831f1592
Vandana <vandana1anand@gmail.com>
2020-05-30 16:50:13 -0400

    Android Java Application

---
+++ Graduate/MIS571/vanand_Tutorial3/Tutorial3_VandanaAnand/app/src/main/java/com/example/tutorial3_vandanaanand/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.tutorial3_vandanaanand;
+
+import com.example.tutorial3_vandanaanand.constant.SQLCommand;
+import com.example.tutorial3_vandanaanand.view.TableView;
+import com.example.tutorial3_vandanaanand.util.DBOperator;
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.ScrollView;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+        //copy database file
+        try{
+            DBOperator.copyDB(getBaseContext());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        //implement SQL query and get cursor of resultset
+        Cursor cursor = DBOperator.getInstance().execQuery(SQLCommand.QUERY_STUDENT);
+        TableView tableView = new TableView(this, cursor);
+        //show data in tableview
+        ScrollView scrollView = (ScrollView)this.findViewById(R.id.scrollView1);
+        scrollView.addView(tableView);
+    }
+}
+
+
+
+//import android.os.Bundle;
+//
+//import com.google.android.material.floatingactionbutton.FloatingActionButton;
+//import com.google.android.material.snackbar.Snackbar;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.widget.Toolbar;
+//
+//import android.view.View;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//}
---
+++ Graduate/MIS571/vanand_Tutorial3/Tutorial3_VandanaAnand/app/src/main/java/com/example/tutorial3_vandanaanand/constant/SQLCommand.java
@@ -0,0 +1,8 @@
+package com.example.tutorial3_vandanaanand.constant;
+
+public abstract class SQLCommand
+{
+    //query all students
+    public static String QUERY_STUDENT = "select stid, stname from Student";
+}
+
